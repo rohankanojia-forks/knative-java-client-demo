3d531705384b7df11d1e024d8f70d7cb342e4478
Rohan Kumar <rohaan@redhat.com>
2020-06-07 19:22:25 +0530

    Fix formatting

--- src/main/java/io/fabric8/knative/client/demo/KnativeRouteCrudDemo.java
+++ src/main/java/io/fabric8/knative/client/demo/KnativeRouteCrudDemo.java
@@ -5,7 +5,6 @@ import io.fabric8.knative.client.KnativeClient;
 import io.fabric8.knative.serving.v1.Route;
 import io.fabric8.kubernetes.api.model.DeletionPropagation;
 
-import java.io.FileInputStream;
 import java.io.InputStream;
 
 public class KnativeRouteCrudDemo {
@@ -13,29 +12,29 @@ public class KnativeRouteCrudDemo {
     public static final String NAMESPACE = "default";
 
     public static void main(String[] args) {
-try (KnativeClient kn = new DefaultKnativeClient()) {
-    // Load Route object from YAML
-    InputStream routeYamlIn = KnativeRouteCrudDemo.class.getResourceAsStream("/knative-route.yml");
-    Route route = kn.routes().load(routeYamlIn).get();
+        try (KnativeClient kn = new DefaultKnativeClient()) {
+            // Load Route object from YAML
+            InputStream routeYamlIn = KnativeRouteCrudDemo.class.getResourceAsStream("/knative-route.yml");
+            Route route = kn.routes().load(routeYamlIn).get();
 
-    // Create Route object into Kubernetes
-    kn.routes().inNamespace(NAMESPACE).createOrReplace(route);
+            // Create Route object into Kubernetes
+            kn.routes().inNamespace(NAMESPACE).createOrReplace(route);
 
-    // Get Route object from APIServer
-    String routeName = route.getMetadata().getName();
-    route = kn.routes().inNamespace(NAMESPACE)
-            .withName(routeName)
-            .get();
+            // Get Route object from APIServer
+            String routeName = route.getMetadata().getName();
+            route = kn.routes().inNamespace(NAMESPACE)
+                    .withName(routeName)
+                    .get();
 
-    // Edit Route object, add some dummy label
-    kn.routes().inNamespace(NAMESPACE).withName(routeName).edit()
-            .editOrNewMetadata()
-               .addToAnnotations("context", "demo")
-            .endMetadata()
-            .done();
+            // Edit Route object, add some dummy label
+            kn.routes().inNamespace(NAMESPACE).withName(routeName).edit()
+                    .editOrNewMetadata()
+                    .addToAnnotations("context", "demo")
+                    .endMetadata()
+                    .done();
 
-    // Delete Route object
-    kn.routes().inNamespace(NAMESPACE).withName(routeName).withPropagationPolicy(DeletionPropagation.FOREGROUND).delete();
-}
+            // Delete Route object
+            kn.routes().inNamespace(NAMESPACE).withName(routeName).withPropagationPolicy(DeletionPropagation.FOREGROUND).delete();
+        }
     }
 }
