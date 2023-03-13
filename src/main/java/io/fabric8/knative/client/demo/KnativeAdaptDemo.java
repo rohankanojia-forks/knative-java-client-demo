aa35c8288f722ee10b55a9a8f990b8cb7ded1b00
Rohan Kumar <rohaan@redhat.com>
2023-03-13 11:13:58 +0530

    deps : Update Fabric8 Knative Client to v6.5.0
    Signed-off-by: Rohan Kumar <rohaan@redhat.com>

--- src/main/java/io/fabric8/knative/client/demo/KnativeAdaptDemo.java
+++ src/main/java/io/fabric8/knative/client/demo/KnativeAdaptDemo.java
@@ -1,6 +1,7 @@
 package io.fabric8.knative.client.demo;
 
 import io.fabric8.knative.client.KnativeClient;
+import io.fabric8.knative.serving.v1.Service;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClientBuilder;
 
@@ -8,8 +9,7 @@ public class KnativeAdaptDemo {
     public static void main(String[] args) {
         try (KubernetesClient client = new KubernetesClientBuilder().build()) {
             KnativeClient kn = client.adapt(KnativeClient.class);
-            if (client.isAdaptable(KnativeClient.class)) {
-            //if (kn.isSupported()) {
+            if (kn.supports(Service.class)) {
                 System.out.println("Client is adaptable to Knative Client, adapting...");
                 kn = client.adapt(KnativeClient.class);
 
